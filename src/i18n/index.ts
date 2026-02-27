7ffedc49e0ae52583b8fc66b9b5632b6c5460320
workliappid-beep <octopilotco@gmail.com>
2026-02-27 08:17:53 +0800

    Implement i18n support with English and Indonesian translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,6 +2,42 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpBackend from 'i18next-http-backend';
+import { createContext, useState } from 'react';
+import { en } from './en';
+import { id } from './id';
+
+export type Lang = 'en' | 'id';
+
+export const translations = { en, id };
+
+export type LanguageContextType = {
+  lang: Lang;
+  t: typeof translations[Lang];
+  setLang: (lang: Lang) => void;
+};
+
+const LanguageContext = createContext<LanguageContextType>({
+  lang: 'en',
+  t: translations['en'],
+  setLang: () => {},
+});
+
+export const LanguageProvider = ({ children }: { children: React.ReactNode }) => {
+  const [lang, setLangState] = useState<Lang>(() => {
+    return (localStorage.getItem('vexlo-lang') as Lang) || 'en';
+  });
+
+  const setLang = (lng: Lang) => {
+    setLangState(lng);
+    localStorage.setItem('vexlo-lang', lng);
+  };
+
+  return (
+    <LanguageContext.Provider value={{ lang, t: translations[lang], setLang }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
 
 i18n
   .use(HttpBackend)
@@ -29,3 +65,4 @@ i18n.on('languageChanged', (lng) => {
 });
 
 export default i18n;
+export { LanguageContext };
